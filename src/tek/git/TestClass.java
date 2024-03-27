074ee5819bb729dac4c1519fc73e9ffe265a42d2
merfani1 <162346858+merfani1@users.noreply.github.com>
2024-03-26 23:09:03 -0400

    Update TestClass.java

--- src/tek/git/TestClass.java
+++ src/tek/git/TestClass.java
@@ -3,5 +3,6 @@ package tek.git;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("Second project");
+        System.out.println("Hello github repository");
     }
 }
