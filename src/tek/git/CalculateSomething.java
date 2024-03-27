1f4f2d5fd317a1a6034ded7422783ff3e7f373d1
merfani1 <murtaza.erfani9@gmail.com>
2024-03-26 22:43:40 -0400

    First changes again

---
+++ src/tek/git/CalculateSomething.java
@@ -0,0 +1,8 @@
+package tek.git;
+
+public class CalculateSomething {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the coding");
+    }
+}
---
+++ src/tek/git/SecondClass.java
@@ -0,0 +1,8 @@
+package tek.git;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Second class");
+        System.out.println(1+2);
+    }
+}
