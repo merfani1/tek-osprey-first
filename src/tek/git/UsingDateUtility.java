24f47dbece0282a26315001b996f141851acbf9f
merfani1 <murtaza.erfani9@gmail.com>
2024-04-03 14:09:44 -0400

    Using Date Utility

---
+++ src/tek/git/UsingDateUtility.java
@@ -0,0 +1,9 @@
+package tek.git;
+
+public class UsingDateUtility {
+    public static void main(String[] args) {
+
+        DateUtility dateUtility=new DateUtility();
+        System.out.println(dateUtility.now());
+    }
+}
