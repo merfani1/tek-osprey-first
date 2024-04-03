67e21e354e80cc12322c224db9ab26d0d395dece
merfani1 <murtaza.erfani9@gmail.com>
2024-04-03 15:11:23 -0400

    changign value

--- src/tek/git/SecondClass.java
+++ src/tek/git/SecondClass.java
@@ -3,6 +3,6 @@ package tek.git;
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("Second class");
-        System.out.println(1+2);
+        System.out.println(5+2);
     }
 }
